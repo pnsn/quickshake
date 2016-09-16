da1c56bb086fd155ab6a05b861f74dae55c222f6
Jon Connolly <jontconnolly@gmail.com>
2016-09-15 21:03:20 -0700

    modified config files, refactored client script, added public files

--- script/secret-example.sh
+++
@@ -1,5 +0,0 @@
-#rename this file to secret.sh and source. secret.sh is gitignored
-export MONGO_USER='SOME_USER'
-export MONGO_PASSWD='Super duper secret'
-export MONGO_HOST='host'
-export MONGO_PORT='27017'
