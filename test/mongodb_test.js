327a4bc979c69d31d25bd9c778b20bc6b074a86f
Jon Connolly <jontconnolly@gmail.com>
2016-10-14 14:53:32 -0700

    add better error handling to archiver, add some tests

--- test/mongodb_test.js
+++ test/mongodb_test.js
@@ -1,31 +1,31 @@
-//test cases for mongoserver prototype
-var assert = require('assert')
-    , Conf = require("../config.js")
-    , RingBuffer = require(__dirname + '/../lib/ringBuffer')
-    , MongoServer = require(__dirname + '/../lib/mongoServer');
-
-
-
-var conf = new Conf();
-var buffMax=6;
-var ringbuff = new RingBuffer(buffMax);
-var ms = new MongoServer(ringbuff);
-
-ms.tail();
-
-//set bufferMax to small number, allow to fill then check length
-var count =10;
-var si = setInterval(function(){
-  for(var scnl in ringbuff['ring']){
-    assert(ringbuff['ring'][scnl].traces.length <= buffMax)
-  }
-  count --;
-  if(count<=0){
-    clearInterval(si);
-    console.log("clearing");
-    process.exit();
-  }
-},1000);
+// //test cases for mongoserver prototype
+// var assert = require('assert')
+//     , Conf = require("../config.js")
+//     , RingBuffer = require(__dirname + '/../lib/ringBuffer')
+//     , MongoServer = require(__dirname + '/../lib/mongoServer');
+//
+//
+//
+// var conf = new Conf();
+// var buffMax=6;
+// var ringbuff = new RingBuffer(buffMax);
+// var ms = new MongoServer(ringbuff);
+//
+// ms.tail();
+//
+// //set bufferMax to small number, allow to fill then check length
+// var count =10;
+// var si = setInterval(function(){
+//   for(var scnl in ringbuff['ring']){
+//     assert(ringbuff['ring'][scnl].traces.length <= buffMax)
+//   }
+//   count --;
+//   if(count<=0){
+//     clearInterval(si);
+//     console.log("clearing");
+//     process.exit();
+//   }
+// },1000);
 
 
 
--- test/ringbuffer_test.js
+++ test/ringbuffer_test.js
@@ -1,46 +1,65 @@
 //test cases for mongoserver prototype
-var assert = require('assert')
-    , Conf = require("../config.js")
-    , RingBuffer = require(__dirname + '/../lib/ringBuffer');
+// var assert = require('assert')
+    var Conf = require("../config.js")
+    ,expect  = require("chai").expect
+    ,RingBuffer = require("../lib/ringBuffer.js");
 
 
 
 var conf = new Conf();
-var buffMax=6;
-var ringbuff = new RingBuffer(buffMax)
-
-
-// //create mock data
-function mockTraceJson(scnl){
+function mockTraceJson(sta,chan,net,loc){
   return {
-    "key": scnl,
+    "key": makeEwKey(sta,chan,net,loc),
     "starttime":  Date.now() -2000,
     "endtime":    Date.now(),
     "samprate":   200,
     "sta":        sta,
-    'chan':       "EHZ",
-    'net':        "UW",
-    'loc':        "--",
-    'data':       [1,3, 100000,-200]
+    'chan':       chan,
+    'net':        net,
+    'loc':        loc,
+    'traces':       [1,3, 100000,-200]
   };
 }
 
 
-
-
-
-var sta="SHIT";
-var scnl = sta + "_" + "EHZ" + "_" + "UW" + "--";
-
-//set bufferMax to small number, fill then check length
-for(var i=0; i< buffMax; i++){
-  ringbuff.update(new mockTraceJson(scnl));
+//return a key as would be found in EW RING (all upcase with '-' deliminators)
+function makeEwKey(sta,chan,net,loc){
+  return sta + "-" + chan + "-" + net + "-" + loc;
 }
-console.log(ringbuff);
-assert(ringbuff['ring'][scnl].traces.length ===buffMax);
-assert(ringbuff['ring'][scnl].currentIndex ===buffMax -1);
-ringbuff.update(new mockTraceJson(scnl));
-assert(ringbuff['ring'][scnl].currentIndex ===0);
 
+describe("Ringbuffer update", function(){
+  var buffMax=6;
+  var ringbuff = new RingBuffer(buffMax);
+  var sta="SHIT";
+  var chan="EHZ";
+  var net="UW";
+  var loc="--";
+  var ewKey=makeEwKey(sta,chan,net,loc);
+  //set bufferMax to small number, fill then check length
+  for(var i=0; i< buffMax; i++){
+    ringbuff.update(new mockTraceJson(sta,chan,net,loc));
+  }
+
+  
+  var validKey= ewKey.replace(/-|\./g, "_").toLowerCase();
+  it("make valid scnl key", function(){
+    expect(ringbuff.makeKey(ewKey)).to.equal(validKey);
+  });
+  console.log(validKey);
+  console.log(ringbuff);
+  it("has valid index", function(){
+    expect(ringbuff['ring'][validKey].traces.length).to.equal(buffMax);
+    expect(ringbuff['ring'][validKey].currentIndex).to.equal(buffMax-1);
+    ringbuff.update(new mockTraceJson(sta,chan,net,loc));
+    expect(ringbuff['ring'][validKey].currentIndex).to.equal(0);
+  
+  });
+});
+// // console.log(ringbuff);
+// assert(ringbuff['ring'][scnl].traces.length ===buffMax);
+// assert(ringbuff['ring'][scnl].currentIndex ===buffMax -1);
+// ringbuff.update(new mockTraceJson(scnl));
+// assert(ringbuff['ring'][scnl].currentIndex ===0);
+//
 
 
