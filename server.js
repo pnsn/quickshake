33f96173f0ea63ddb52845c33de62cb1e8f48aee
Jon Connolly <jontconnolly@gmail.com>
2016-12-19 13:04:13 -0800

    change endtime to duration

--- server.js
+++ server.js
@@ -115,14 +115,15 @@ app.get("/archive", function(req, res) {
            clean_scnls.push(scnls[i]);
          }
        }
-       var endtime;
-       if(req.query.endtime===undefined || 
-         parseInt(req.query.endtime,0) > (60*60*1000) || 
-         parseInt(req.query.endtime,0) < (1*60*1000)){
-            endtime= starttime + (10*60*1000);
-         }else{
-           endtime=request.endtime;
-         }
+       var duration;
+       
+       if(req.query.duration===undefined || 
+           parseInt(req.query.duration,0) > (60*60*1000) || 
+           parseInt(req.query.duration,0) < (1*60*1000)){
+         endtime= starttime + (10*60*1000);
+       }else{
+         endtime=startime + req.query.duration;
+       }
        var results=[];
        logger.info("starttime: " + starttime + " endtime: " + endtime + " scnls:" + clean_scnls);
        sendArchive(clean_scnls, res, starttime, endtime, results);
