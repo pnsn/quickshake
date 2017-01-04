0089f0869b3107fc47ea06be7f4fef25a007cbca
Jon Connolly <jontconnolly@gmail.com>
2017-01-04 14:31:26 -0800

    add root stuff to the root thing

--- script/aws/after_install-root.sh
+++ script/aws/after_install-root.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-exit 0
+set -e
+set -o pipefail
+#give this shat back to ec2-user
+rm -rf /var/www/quickshake
+mv /var/www/tmp /var/www/quickshake
+chown -R ec2-user:ec2-user  /var/www/quickshake
