c9561a66cc8c83a1cbd7f569828c8a6b19a65650
Kyla Marczewski <kyla.m.ski@gmail.com>
2017-01-31 09:57:50 -0800

    added insurance against bad station

--- public/javascript/quickshake.js
+++ public/javascript/quickshake.js
@@ -1352,7 +1352,7 @@ $(function() {
           if(stations[sta] && $.inArray(cha, stations[sta].chans) === -1){
             stations[sta].chans.push(cha);
             stations[sta].scnls.push(scnl);
-          } else {
+          } else if(cha != "TE") {
             stations[sta] = {
               sta: sta,
               net : net,
