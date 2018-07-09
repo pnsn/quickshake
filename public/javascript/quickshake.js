ba9fe3d8bb7ba522321543d6d258404ac92428e9
Kyla Marczewski <marczk@uw.edu>
2018-07-09 11:19:06 -0700

    removed hardcoding

--- public/javascript/quickshake.js
+++ public/javascript/quickshake.js
@@ -700,8 +700,8 @@ $(function() {
     right: -115,
     mag: 3
   };
-  var path = "quickshake.pnsn.org/";
-  // var path = window.location.host + "/";
+  // var path = "quickshake.pnsn.org/";
+  var path = window.location.host + "/";
   var usgsPath = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&";
 
   // Initialize UI
