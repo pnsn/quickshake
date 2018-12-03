bbbc09bc1635cacfa23cad5950248b35e7d19950
Jon Connolly <jontconnolly@gmail.com>
2018-12-03 13:59:37 -0800

    add port range to config

--- config/waveserver.conf.js
+++ config/waveserver.conf.js
@@ -1,7 +1,9 @@
 function WaveServerConf(){
-
-  this.waveHost = "ewserver1.ess.washington.edu";
-  this.wavePort = 16021;
+  //Enter port range (inclusive) of waveservers. Process starts at lowest port and
+  //iterates through till it finds a hit or runs out of port
+  this.waveHost = "mazama.ess.washington.edu";
+  this.wavePortStart = 16017;
+  this.wavePortEnd = 16017;
 }
 
 module.exports = WaveServerConf;
