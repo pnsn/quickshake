96e7a5240c15a7dd454db9017b3a4f75d1392dda
Jon Connolly <jontconnolly@gmail.com>
2020-08-04 14:02:13 -0700

    cherry pic files off of master

--- script/production/scnl_update.js
+++ script/production/scnl_update.js
@@ -1,8 +1,7 @@
 /*
-*script to populate scnl table. --src=iris will get data from iris
-* --src=path/to/jsonfile will get obj from json
+*script to populate scnl table with IRIS webservice data.
 * envoke with
-* node script/production/scnl_update.js --src[iris|path/to/jsonfile]
+* node script/production/scnl_update.js
 */
 
 'use strict';
@@ -11,24 +10,21 @@ const scnlConf = require("../../config/scnlConf.js");
 const serverConf = require("../../config/serverConf.js");
 const scnlconf = new scnlConf();
 const serverconf= new serverConf();
-// const logger = require('winston');
 const MongoClient  = require('mongodb').MongoClient;
 const Scnl = require("../../lib/scnl.js");
 var env=process.env.NODE_ENV || "development"; //get this from env
 var MONGO_URI = serverconf[env].mongo.uri;
 var DB_NAME = serverconf[env].mongo.dbName;
-var fs = require("fs");
-const args = require('yargs').argv;
-var usage="must specify src=iris or filepath";
 
 var scnl =Scnl;
-var collName= "scnl";
+var collName= "scnl"; // mongo collection
+
 MongoClient.connect(MONGO_URI, function(err, client) {
   if(err) throw err;
   const db = client.db(DB_NAME);
 
   scnl.parseIrisScnls(scnlconf.nets, function(err, iris_scnls, response){
-    scnl.getCollections(db, function(err, scnls){
+    scnl.getCwaveCollections(db, function(err, scnls){
       for(var i=0; i<scnls.length; i++){
         if(iris_scnls.hasOwnProperty(scnls[i])){
           scnl.upsert(db,iris_scnls[scnls[i]], function(err, results){
